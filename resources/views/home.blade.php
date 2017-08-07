1c84e8355076a7fd47c9eba82ce3e321a4170c2d
SadCreeper <87826632@qq.com>
2017-08-07 18:08:56 +0800

    init the base blade

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title', '首页')
+
+@section('content')
+<!-- 最新文章 -->
+<div class="row">
+    <div class="col-md-12">
+        This is home
+    </div>
+</div>
+@endsection 
---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+{{-- 这是底部信息栏 --}}
+<footer style="background-color:gray">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <p>This is the footer</p>
+            </div>
+        </div>
+    </div>
+</footer>
