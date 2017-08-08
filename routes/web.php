cd6e4b20161af45bf1e9a1778ecda7a71f5a788c
SadCreeper <87826632@qq.com>
2017-08-08 09:37:10 +0800

    add users route

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,14 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-
-
-Auth::routes();
-
-// Route::get('/home', 'HomeController@index');
+//用户资源路由
+Route::get('/users', 'UsersController@index')->name('users.index');
+Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+Route::get('/users/create', 'UsersController@create')->name('users.create');
+Route::post('/users', 'UsersController@store')->name('users.store');
+Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
 
 //首页
 Route::get('/','PagesController@home')->name('home');
