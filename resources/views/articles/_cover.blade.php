7697b878709213f9489930aa1f4ff800d471b42d
SadCreeper <87826632@qq.com>
2017-09-09 15:57:16 +0800

    update home cover styles in phone

--- resources/views/articles/_cover.blade.php
+++ resources/views/articles/_cover.blade.php
@@ -2,7 +2,7 @@
     <a href="{{ route('articles.show', $article->id) }}"><img src="{{ $article->cover }}" alt="" class="img-responsive"></a>
 
     <div class="hidden-lg hidden-md hidden-sm">
-        <p class="z-img-bottom-bar"><span class="glyphicon glyphicon-play-circle"></span> {{ $article->view }} <span class="glyphicon glyphicon-edit" style="margin-left:5px"></span> {{ $article->comment }}</p>
+        <p class="z-img-bottom-bar"><span class="glyphicon glyphicon-play-circle" style="margin-left:5px"></span> {{ $article->view }} <span class="glyphicon glyphicon-edit" style="margin-left:5px"></span> {{ $article->comment }}</p>
         <p style="height:44px;overflow:hidden;margin-bottom:0">{{ $article->title }}</p>
     </div>
 </div>
