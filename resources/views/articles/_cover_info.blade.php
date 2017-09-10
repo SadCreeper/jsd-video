c605fe4ab80430bf296b363fc269c5ea965bdb56
SadCreeper <87826632@qq.com>
2017-09-10 17:16:02 +0800

    update article category layout

--- resources/views/articles/_cover_info.blade.php
+++ resources/views/articles/_cover_info.blade.php
@@ -5,7 +5,7 @@
             <a href="{{ route('articles.show', $article->id) }}"><img src="{{ $article->cover }}" alt="" class="img-responsive"></a>
         </div>
         <div class="col-md-6">
-            <a href="{{ route('articles.show', $article->id) }}"><p style="white-space: nowrap;overflow:hidden"><b>{{ $article->title }}</b></p></a>
+            <a href="{{ route('articles.show', $article->id) }}"><p style="margin-bottom:0;white-space: nowrap;overflow:hidden"><b>{{ $article->title }}</b></p></a>
             <p style="color:#BEBEBE;max-height:44px;overflow:hidden;">{{ $article->intro }}</p>
             <p><span class="glyphicon glyphicon-play-circle"></span> {{ $article->view }} <span class="glyphicon glyphicon-edit"></span> {{ $article->comment }}</p>
         </div>
