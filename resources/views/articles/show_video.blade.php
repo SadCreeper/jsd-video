38abc0b830b6f7eb195a97d78121a5752d07eca4
SadCreeper <87826632@qq.com>
2017-10-05 17:53:28 +0800

    article show-video page: remove the fake html & comment replace

--- resources/views/articles/show_video.blade.php
+++ resources/views/articles/show_video.blade.php
@@ -14,8 +14,8 @@
             </video>
         </div>
         <div class="col-md-4">
-            <h4>18798 人正在看</h4>
-            <p style="background-color:#CFCFCF">01:51 我要 这变化又如何</p>
+            <h4 style="border-bottom:1px solid #d6d4cf;padding-bottom:10px;margin-top:0">热门评论</h4>
+            <!-- <p style="background-color:#CFCFCF">01:51 我要 这变化又如何</p>
             <p style="background-color:#CFCFCF">00:24 月溅星河</p>
             <p style="background-color:#CFCFCF">00:28 月溅星河，长夜漫漫</p>
             <p style="background-color:#CFCFCF">01:34 大圣是我永恒的男神！</p>
@@ -24,7 +24,8 @@
             <p style="background-color:#CFCFCF">00:24 月溅星河</p>
             <p style="background-color:#CFCFCF">00:28 月溅星河，长夜漫漫</p>
             <p style="background-color:#CFCFCF">01:34 大圣是我永恒的男神！</p>
-            <p style="background-color:#CFCFCF">01:52 我有这变化又如何</p>
+            <p style="background-color:#CFCFCF">01:52 我有这变化又如何</p> -->
+            <p style="font-size:12px;color:gray;margin-top:30px"> -- 暂无评论 -- </p>
         </div>
     </div>
     <div class="row">
